a562fe2989cd3885dbb0b9bdfb62d2e5ff1e0755
--replace-all <samma118@hotmail.com>
2023-09-05 22:59:16 -0400

    Set up sign in feature

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,17 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    }
+    setProviders();
+  }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -45,6 +55,17 @@ const Nav = () => {
           </div>
         ) : (
           <>
+            {providers &&
+            Object.values(providers).map((provider) => (
+              <button
+                type="button"
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className="black_btn"
+              >
+                Sign In
+              </button>
+            ))}
           </>
         )}
       </div>
