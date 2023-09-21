a473bbc8df6a775195b22a34a0520c4244b5aef9
--replace-all <samma118@hotmail.com>
2023-09-21 00:23:40 -0400

    Fix db connection bug

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,41 +12,44 @@ const handler = NextAuth({
     })
   ],
 
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email
-    })
-
-    session.user.id = sessionUser._id.toString();
-
-    return session;
-  },
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email
+      })
   
-  async signIn({ profile }) {
-    try {
-      // serverless -> lambda -> dynamodb
-      await connectToDB();
-
-      // check if an user already exists
-      const userExists = await User.findOne({
-        email: profile.email
-      });
-
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture
-        })
+      session.user.id = sessionUser._id.toString();
+  
+      return session;
+    },
+    
+    async signIn({ profile }) {
+      try {
+        // serverless -> lambda -> dynamodb
+        await connectToDB();
+  
+        // check if an user already exists
+        const userExists = await User.findOne({
+          email: profile.email
+        });
+  
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture
+          })
+        }
+  
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
     }
   }
+
 })
 
 export { handler as GET, handler as POST };
