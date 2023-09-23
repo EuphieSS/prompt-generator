93647293fd15e5b3aa2d511537959cfb038e5ff3
--replace-all <samma118@hotmail.com>
2023-09-22 23:34:18 -0400

    Fix minor bugs and typos

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,22 +1,16 @@
-// API Route
-
 import { connectToDB } from "@utilities/database";
 import Prompt from "@models/prompt";
 
-export const POST = async (req, res) => {
+export const POST = async (req) => {
   const { userId, prompt, tag } = await req.json();
 
   try {
-    await connectToDB();
-    const newPrompt = new Prompt({
-      creator: userId,
-      tag
-    });
+      await connectToDB();
+      const newPrompt = new Prompt({ creator: userId, prompt, tag });
 
-    await newPrompt.save();
-
-    return new Response(JSON.stringify(newPrompt), { status: 201 })
+      await newPrompt.save();
+      return new Response(JSON.stringify(newPrompt), { status: 201 });
   } catch (error) {
-    return new Response("Failed to create a new prompt", { status: 500 })
+      return new Response("Failed to create a new prompt", { status: 500 });
   }
 }
