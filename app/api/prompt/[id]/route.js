41cc379079b0471cd6e5c6b72095b65e28980b8b
--replace-all <samma118@hotmail.com>
2023-09-26 19:56:29 -0400

    Add endpoint to update prompt by id

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -18,5 +18,27 @@ export const GET = async (req, { params }) => {
 }
 
 // PATCH (update)
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.JSON();
+
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update prompt", { status: 500 });
+  }
+}
 
 // DELETE (delete)
