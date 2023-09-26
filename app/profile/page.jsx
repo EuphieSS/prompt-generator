8aef15571ec75138f7eac61518af0b6dc2d3b5b0
--replace-all <samma118@hotmail.com>
2023-09-26 19:02:35 -0400

    Add useEffect to fetch prompts by user

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,6 +7,25 @@ import { useRouter } from 'next/navigation';
 import Profile from '@components/Profile';
 
 const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect (() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    }
+
+    console.log(posts);
+
+    if (session?.user.id) {
+      fetchPosts();
+    }
+  }, [])
+
   const handleEdit = () => {
 
   }
@@ -19,7 +38,7 @@ const MyProfile = () => {
     <Profile
       name="My"
       desc="Welcome to your personalized profile page"
-      data={[]}
+      data={posts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
