4d93db10fb0e9e2c75cae53515d37cb0b9f8af20
--replace-all <samma118@hotmail.com>
2023-09-26 19:54:33 -0400

    Add function to fetch prompt by id

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,4 +1,21 @@
+import { connectToDB } from "@utilities/database";
+import Prompt from "@models/prompt";
+
 // GET (read)
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    const prompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", { status: 500 });
+  }
+}
 
 // PATCH (update)
 
