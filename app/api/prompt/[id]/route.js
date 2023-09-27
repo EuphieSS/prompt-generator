1020376b8c91de6bd299beab60168e4a6720a0ae
--replace-all <samma118@hotmail.com>
2023-09-26 20:01:44 -0400

    Add endpoint to delete prompt by id

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -42,3 +42,14 @@ export const PATCH = async (req, { params }) => {
 }
 
 // DELETE (delete)
+export const DELETE = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    await Prompt.findByIdAndRemove(params.id);
+
+    return new Response("Prompt deleted successfully", { status: 200 })
+  } catch (error) {
+    return new Response("Failed to delete prompts", { status: 500 });
+  }
+}
